fe5283f76c122f11fff1735fc0324093478bd680
volodya4u <volodya4u@hotmail.com>
2026-01-02 01:55:48 +0200

    Add JpaTransactionManager

--- src/main/java/com/appsdeveloperblog/estore/transfers/KafkaConfig.java
+++ src/main/java/com/appsdeveloperblog/estore/transfers/KafkaConfig.java
@@ -3,6 +3,7 @@ package com.appsdeveloperblog.estore.transfers;
 import java.util.HashMap;
 import java.util.Map;
 
+import jakarta.persistence.EntityManagerFactory;
 import org.apache.kafka.clients.admin.NewTopic;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.springframework.beans.factory.annotation.Value;
@@ -13,6 +14,7 @@ import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
 import org.springframework.kafka.transaction.KafkaTransactionManager;
+import org.springframework.orm.jpa.JpaTransactionManager;
 
 @Configuration
 public class KafkaConfig {
@@ -84,6 +86,11 @@ public class KafkaConfig {
 	@Bean("kafkaTransactionManager")
 	KafkaTransactionManager<String, Object> kafkaTransactionManager(ProducerFactory<String, Object> producerFactory) {
 		return new KafkaTransactionManager<>(producerFactory);
+	}
+
+	@Bean("transactionManager")
+	JpaTransactionManager jpaTransactionManager(EntityManagerFactory entityManagerFactory) {
+		return new JpaTransactionManager(entityManagerFactory);
 	}
 
 	@Bean
